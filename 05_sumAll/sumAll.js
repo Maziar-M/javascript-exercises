2c4ab201885ef577ac34b67736e94db2710334b1
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-22 14:42:24 +0330

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    for (let arg of arguments) {
+        if (typeof arg !== "number" || arg < 0) {
+            return "ERROR";
+        }
+    }
 
+    let range = Array.from(arguments);
+    range.sort((num1, num2) => num1 - num2);
+
+    let sum = 0;
+    for (let num = range[0]; num <= range[1]; num++) {
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
