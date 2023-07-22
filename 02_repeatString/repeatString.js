e9e8aabcff8463556bf3e9d4ce5fa3fa6aaba682
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-22 11:41:22 +0330

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(originalString, repeatTimes) {
+    if (repeatTimes < 0) {
+        return "ERROR";
+    }
+    
+    let repeatedString = "";
+    for (let i = 0; i < repeatTimes; i++) {
+        repeatedString += originalString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
