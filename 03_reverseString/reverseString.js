7d0d1d30d58db8f7530d1252f79088e5d4a2fe36
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-22 12:29:15 +0330

    Solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let splittedString = originalString.split("");
+    let reversedString = "";
+    for (let i = splittedString.length - 1; i >= 0; i--) {
+        reversedString += splittedString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
