42a5fdfdf4edfc1c5eadc869e136ed6c87b625aa
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-22 14:03:56 +0330

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...toBeRemoved) {
+    for (let removableIndex = 0; removableIndex < toBeRemoved.length; removableIndex++) {
+        for (let arrayIndex = 0; arrayIndex < originalArray.length; arrayIndex++) {
+            if (toBeRemoved[removableIndex] === originalArray[arrayIndex]) {
+                originalArray.splice(arrayIndex, 1);
+            }
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
