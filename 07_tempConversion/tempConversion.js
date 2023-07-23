9ba1045c8f4f5aa6a5acacd1b9fad13c9a90d624
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-23 10:56:57 +0330

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let degreeC = (degreeF - 32) * (5 / 9);
+  degreeC = Math.round(degreeC * 10) / 10;
+  return degreeC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let degreeF = (degreeC * 9 / 5) + 32;
+  degreeF = Math.round(degreeF * 10) / 10;
+  return degreeF;
 };
 
 // Do not edit below this line
