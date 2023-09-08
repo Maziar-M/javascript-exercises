645272a1d3cd3769335935a47b31e96dff079a8d
Maziar-M <maziar.mir.2002@gmail.com>
2023-09-08 15:19:13 +0330

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthMember) {
+  if (nthMember < 0) return 'OOPS';
+  if (nthMember == 0) return 0;
+  if (nthMember == 1) return 1;
+  return fibonacci(nthMember - 1) + fibonacci(nthMember - 2);
 };
 
 // Do not edit below this line
