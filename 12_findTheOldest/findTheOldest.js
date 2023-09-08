956bf42fd332e74571a64b909bd14f111c7d15b8
Maziar-M <maziar.mir.2002@gmail.com>
2023-09-08 17:36:04 +0330

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const currentPersonAge = ('yearOfDeath' in currentPerson) ?
+                             (currentPerson.yearOfDeath - currentPerson.yearOfBirth)
+                             :
+                             ((new Date().getFullYear()) - currentPerson.yearOfBirth);
 
+    const oldestPersonAge = ('yearOfDeath' in oldestPerson) ?
+                             (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)
+                             :
+                             ((new Date().getFullYear()) - oldestPerson.yearOfBirth);
+
+    return (currentPersonAge > oldestPersonAge) ? currentPerson : oldestPerson;
+  });
 };
 
 // Do not edit below this line
