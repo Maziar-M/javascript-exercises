60908c4264fdc09586488a11b00b07ff55c629a6
Maziar-M <maziar.mir.2002@gmail.com>
2023-09-08 13:17:14 +0330

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const processedString = string.replace(/\W|_/g, '').replaceAll(' ', '').toLowerCase();
+  return processedString === processedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
